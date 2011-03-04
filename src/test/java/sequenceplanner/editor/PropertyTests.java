a92492b46fc59d57d5e0176665e585a95f6bd7cf
patrikm <patrikm@chalmers.se>
2011-03-04 17:53:01 +0100

    Patrik is testing

---
+++ src/test/java/sequenceplanner/editor/PropertyTests.java
@@ -0,0 +1,88 @@
+package sequenceplanner.editor;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import sequenceplanner.general.SP;
+import static org.junit.Assert.*;
+
+/**
+ * To test properties
+ * @author patrik
+ */
+public class PropertyTests {
+
+    public PropertyTests() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testtest() {
+        SP sp = new SP();
+        sp.loadFromTemplateSOPXFile("resources/filesForTesting/fileForTesting.sopx");
+        sp.insertOperation();
+        sp.saveToSOPXFile("C:/Users/patrik/Desktop/result.sopx");
+    }
+
+    /**
+     * test of id 100
+     */
+    @Test
+    public void id100() {
+
+        //Insert property (A) with name Adam
+        //Insert property (B) with name Bertil
+
+        //assertTrue(all property names are different);
+
+        //Insert propery (C) with name empty
+        //assertTrue(nbr of properties == 2)
+
+        //Insert property (D) with name Ad
+        //assertTrue(nbr of properties == 3)
+
+        //Change name of B to empty
+        //assertTrue(nbr of properties == 3)
+        //assertTrue(A.getName.equals("Adam"))
+        //assertTrue(B.getName.equals("Bertil"))
+        //assertTrue(C.getName.equals("Ad"))
+
+        //Change name of B to Adam
+        //assertTrue(nbr of properties == 3)
+        //assertTrue(A.getName.equals("Adam"))
+        //assertTrue(B.getName.equals("Bertil"))
+        //assertTrue(C.getName.equals("Ad"))
+
+        //Insert value (1) with name ett to A
+        //Insert value (2) with name två to A
+        //assertTrue(A."nbr of values" == 2)
+
+        //Change name of 1 to empty
+        //assertTrue(A."nbr of values" == 2)
+        //assertTrue(1.getName.equals("ett"))
+        //assertTrue(2.getName.equals("två"))
+
+        //Change name of 1 to två
+        //assertTrue(A."nbr of values" == 2)
+        //assertTrue(1.getName.equals("ett"))
+        //assertTrue(2.getName.equals("två"))
+
+        //Insert value (3) with name ett to B
+        //assertTrue(B."nbr of values" == 1)
+        //assertTrue(3.getName.equals("ett"))
+        //assertTrue(A."nbr of values" == 2)
+        //assertTrue(1.getName.equals("ett"))
+        //assertTrue(2.getName.equals("två"))
+
+        //Insert value (4) with name empty to C
+        //assertTrue(C."nbr of values" == 0)
+    }
+
+}
---
+++ src/test/java/sequenceplanner/general/SP.java
@@ -0,0 +1,105 @@
+package sequenceplanner.general;
+
+import java.io.File;
+import javax.swing.JOptionPane;
+import sequenceplanner.gui.controller.GUIController;
+import sequenceplanner.gui.model.GUIModel;
+import sequenceplanner.gui.view.GUIView;
+import sequenceplanner.model.Model;
+import sequenceplanner.model.TreeNode;
+import sequenceplanner.model.data.OperationData;
+import sequenceplanner.SequencePlanner;
+import static org.junit.Assert.*;
+
+/**
+ * Help-class for testing.<br\>
+ * Creates an instance of SP.<br\>
+ * @author patrik
+ */
+public class SP {
+
+    /**
+     * Hold info about all that is shown in the GUIView.<br/>
+     * see {@link GUIModel guiModel}
+     */
+    private GUIModel guiModel;
+    /**
+     * Main view class for the gui package.<br/>
+     * see {@link GUIView guiView}
+     */
+    private GUIView guiView;
+    /**
+     * Main controller in the GUI package.<br/>
+     * see {@link GUIController guiController}
+     */
+    private GUIController guiController;
+    /**
+     * This class will be specialized to keep operations, liasons and resources.<br/>
+     * see {@link Model model}
+     */
+    private Model model;
+
+    public SP() {
+        guiModel = new GUIModel();
+        model = guiModel.getModel();
+        guiView = new GUIView(guiModel);
+        guiController = new GUIController(guiModel, guiView);
+    }
+
+    /**
+     * @return {@link GUIModel guiModel}
+     */
+    public GUIModel getGUIModel() {
+        return guiModel;
+    }
+
+    /**
+     * @return {@link Model model}
+     */
+    public Model getModel() {
+        return model;
+    }
+
+    /**
+     * Loads {@link Model model} from .sopx-file. Uses xml unmarshaller in {@link GUIController guiController}.<br/>
+     * @param nameOfFile name of .sopx-file
+     */
+    public void loadFromSOPXFile(String nameOfFile) {
+        assertTrue(guiController.openModel(new File(nameOfFile)));
+    }
+
+    /**
+     * Loads {@link Model model} from a template.sopx-file.<br/>
+     * @param nameOfFile name of template.sopx-file
+     */
+    public void loadFromTemplateSOPXFile(String nameOfFile) {
+        loadFromSOPXFile(SequencePlanner.class.getResource(nameOfFile).getFile());
+    }
+
+    /**
+     * Saves {@link Model model} to .sopx-file. Uses xml marshaller in {@link GUIController guiController}.<br/>
+     * @param nameOfFile name of .sopx-file
+     */
+    public void saveToSOPXFile(String nameOfFile) {
+        assertTrue(guiController.saveModelToFile(nameOfFile) instanceof File);
+    }
+
+    /**
+     * Ugly fix to see current layout (window arrangement).
+     */
+    public void visualizeGUI() {
+        GUIView view = new GUIView(guiModel);
+        JOptionPane.showMessageDialog(view, "Hello world");
+    }
+
+    /**
+     * Inserts a new operation as child to root of operation-tree
+     */
+    public void insertOperation() {
+        Integer idCounter = model.getCounter();
+        ++idCounter;
+        OperationData opData = new OperationData("OP" + idCounter, idCounter);
+        model.getOperationRoot().insert(new TreeNode(opData));
+        model.setCounter(idCounter);
+    }
+}
