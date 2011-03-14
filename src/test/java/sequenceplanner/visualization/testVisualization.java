3b82ad07c4c2a5495c85279c2510170272d561db
patrikm <patrikm@chalmers.se>
2011-03-14 18:41:00 +0100

    Test methods used for visualization

---
+++ src/test/java/sequenceplanner/visualization/testVisualization.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sequenceplanner.visualization;
+
+import sequenceplanner.general.SP;
+
+/**
+ *
+ * @author patrik
+ */
+public class testVisualization {
+    SP sp = new SP();
+
+    public void test1() {
+    sp.loadFromTemplateSOPXFile("KristoferTASEexample.sopx");
+
+    }
+
+}
