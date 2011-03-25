713ceb4f58ce4fc8cf841dcdc6e162c5bbc18478
shoaei <shoaei@chalmers.se>
2011-03-25 13:09:59 +0100

    1- Minor changes 2- Adding test package

--- src/main/java/sequenceplanner/efficientModel/OperationSequences.java
+++ src/main/java/sequenceplanner/efficientModel/OperationSequences.java
@@ -5,8 +5,9 @@
 
 package sequenceplanner.efficientModel;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Set;
+import java.util.List;
 import org.apache.log4j.Logger;
 import sequenceplanner.model.Model;
 import sequenceplanner.model.TreeNode;
@@ -18,33 +19,40 @@ import sequenceplanner.model.data.OperationData;
  */
 public class OperationSequences {
     static Logger log = Logger.getLogger(OperationSequences.class);
-    private Model model = null;
-
+    private Model model;
+    private List<TreeNode> operationList;
 
     public OperationSequences(Model model){
         this.model = model;
+        operationList = new ArrayList<TreeNode>();
         init();
     }
 
     public void run(){
-        print("Here");
-        for (int i = 0; i < model.getOperationRoot().getChildCount(); ++i) {
-            OperationData opData = (OperationData) model.getOperationRoot().getChildAt(i).getNodeData();
-            print("**********");
-            print(opData.getPrecondition());
-            LinkedList<LinkedList<Integer>> predecessors = opData.getPredecessors();
-
-            for(LinkedList<Integer> l1 : predecessors){
-                print("----OR----");
-                for(Integer l2 : l1)
-                    print(l2);
-            }
-        }
+        print(operationList.size());
+//        print("Here");
+//        for (int i = 0; i < model.getOperationRoot().getChildCount(); ++i) {
+//            OperationData opData = (OperationData) model.getOperationRoot().getChildAt(i).getNodeData();
+//            print("**********");
+//            print(opData.getPrecondition());
+//            LinkedList<LinkedList<Integer>> predecessors = opData.getPredecessors();
+//
+//            for(LinkedList<Integer> l1 : predecessors){
+//                print("----OR----");
+//                for(Integer l2 : l1)
+//                    print(l2);
+//            }
+//        }
     }
 
     private void init() {
-
+        print("***************************");
+        for (int i = 0; i < model.getOperationRoot().getChildCount(); ++i) {
+            operationList.add(model.getOperationRoot().getChildAt(i));
+        }
+        print("operationList.size()");
     }
+
     private void print(Object o){
         System.err.println(o);
     }
