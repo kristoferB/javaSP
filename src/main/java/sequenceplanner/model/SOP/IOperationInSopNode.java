098752cbf8052a8c8675c1670e5ec4fa6707b11e
patrikm <patrikm@chalmers.se>
2011-04-06 16:21:50 +0200

    not needed anymore

--- src/main/java/sequenceplanner/model/SOP/IOperationInSopNode.java
+++
@@ -1,9 +0,0 @@
-package sequenceplanner.model.SOP;
-
-/**
- * Interface for the connection between operation data structure and {@link ISopNode}.<br/>
- * @author patrik
- */
-public interface IOperationInSopNode {
-
-}
