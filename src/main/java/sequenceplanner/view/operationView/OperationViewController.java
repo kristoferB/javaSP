1dd13afb303a248defe937611ffdb51d0dd7327a
Evelina Davidsson <evelinad@student.chalmers.se>
2011-05-01 16:08:25 +0200

    Developed OperationViewController

--- src/main/java/sequenceplanner/view/operationView/OperationViewController.java
+++ src/main/java/sequenceplanner/view/operationView/OperationViewController.java
@@ -1,22 +1,24 @@
-
 package sequenceplanner.view.operationView;
 
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Observer;
+import sequenceplanner.model.data.Data;
 import sequenceplanner.model.data.OperationData;
+import sequenceplanner.view.operationView.graphextension.Cell;
 
 /**
  * Observes the model and updates all existing operation views when the model is changed
  *
  * @author Evelina
  */
-public class OperationViewController implements Observer{
+public class OperationViewController implements Observer {
 
     //SPGraphModels of all exsting operation views
     private LinkedList<OperationView> views;
 
-    public OperationViewController(){
+    public OperationViewController() {
         views = new LinkedList();
     }
 
@@ -24,21 +26,30 @@ public class OperationViewController implements Observer{
      * Add an SPGraphModel for a operation view
      *
      */
-    public void addOperationView(OperationView v){
+    public void addOperationView(OperationView v) {
         views.add(v);
     }
 
     @Override
     public void update(Observable o, Object arg) {
 
-        if(arg instanceof OperationData){
+        if (arg instanceof OperationData) {
             OperationData od = (OperationData) arg;
 
-            for(OperationView v : views){
+            for (OperationView operationView : views) {
                 //if operation view contains od with this id, update od
+
+                Hashtable cells = operationView.getGraphModel().getCells();
+                for(int i = 2; i < cells.size(); i++){
+                    Cell c = (Cell) cells.get(Integer.toString(i));
+                    if (c.getValue() instanceof OperationData) {
+                        OperationData data = (OperationData) c.getValue();
+                        if (data.getId() == od.getId()) {
+                            operationView.getGraph().setValue(c, data);
+                        }
+                    }
+                }
             }
         }
     }
-
-
 }
