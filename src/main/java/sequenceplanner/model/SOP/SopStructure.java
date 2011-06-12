0183e70d39a3ac79311b132c5d0ea13307c90006
petkle <pbdq_2@hotmail.com>
2011-06-12 19:44:04 +0200

    Comments

--- src/main/java/sequenceplanner/model/SOP/SopStructure.java
+++ src/main/java/sequenceplanner/model/SOP/SopStructure.java
@@ -3,12 +3,19 @@ package sequenceplanner.model.SOP;
 /**
  *
  * @author Qw4z1
+ * *Till viktor*
+ * Vi måste ha en Linked List för varje sekvens där ny root "Before ->Operation"
+ * läggs till som ny "addFirst". Läggs en ny operation till "after" så läggs den
+ * i sist i listan. Läggs en parallell eller alternativ till så måste de länkas
+ * ihop i en annan lista via listan.
+ *
  */
 public class SopStructure implements ISopStructure{
+    private ISopNode node;
 
     @Override
     public void addNode(ISopNode node) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
 
     @Override
