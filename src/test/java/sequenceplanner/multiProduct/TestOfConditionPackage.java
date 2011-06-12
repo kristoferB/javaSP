aa1421cd6bf33e71335618ae96f382b8389f3dee
patrikm <patrikm@chalmers.se>
2011-06-12 19:45:56 +0200

    Started to impement Condition package into SOP->Condtion class.

--- src/test/java/sequenceplanner/multiProduct/TestOfConditionPackage.java
+++ src/test/java/sequenceplanner/multiProduct/TestOfConditionPackage.java
@@ -4,7 +4,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import sequenceplanner.condition.Condition;
-import sequenceplanner.condition.ConditionElement;
 import sequenceplanner.condition.ConditionExpression;
 import sequenceplanner.condition.ConditionOperator;
 import sequenceplanner.condition.ConditionStatment;
@@ -30,25 +29,43 @@ public class TestOfConditionPackage {
 
     @Test
     public void method1() {
-        OperationData op1 = sp.insertOperation();
-        OperationData op2 = sp.insertOperation();
+        final OperationData op1 = sp.insertOperation();
+        final OperationData op2 = sp.insertOperation();
+        final OperationData op3 = sp.insertOperation();
+        final OperationData op4 = sp.insertOperation();
+        final OperationData op5 = sp.insertOperation();
 
+        //How to get (op1006 and op1007) or (op1008 and (op1009 or op1010)) as guard
+
+        //Left clause------------------------------------------------------------
+        ConditionExpression left = new ConditionExpression();
+        ConditionStatment cs1 = new ConditionStatment(Integer.toString(op1.getId()), ConditionStatment.Operator.Equal, "2");
+        ConditionStatment cs2 = new ConditionStatment(Integer.toString(op2.getId()), ConditionStatment.Operator.Equal, "2");
+        left.changeExpressionRoot(cs1);
+        left.appendElement(ConditionOperator.Type.AND, cs2);
+        //-----------------------------------------------------------------------
+
+        //Right clause------------------------------------------------------------
+        ConditionStatment cs4 = new ConditionStatment(Integer.toString(op4.getId()), ConditionStatment.Operator.Equal, "2");
+        ConditionExpression subRight = new ConditionExpression(cs4);
+        ConditionStatment cs5 = new ConditionStatment(Integer.toString(op5.getId()), ConditionStatment.Operator.Equal, "2");
+        subRight.appendElement(ConditionOperator.Type.OR, cs5);
+
+        ConditionExpression right = new ConditionExpression();
+        ConditionStatment cs3 = new ConditionStatment(Integer.toString(op3.getId()), ConditionStatment.Operator.Equal, "2");
+
+        right.changeExpressionRoot(cs3);
+        right.appendElement(ConditionOperator.Type.AND, subRight);
+        //-----------------------------------------------------------------------
+
+        //Merge left and right---------------------------------------------------
         Condition c = new Condition();
         ConditionExpression ce = c.getGuard();
-        ConditionStatment cs1 = new ConditionStatment(Integer.toString(op1.getId()), ConditionStatment.Operator.NotEqual, "1");
-        ConditionStatment cs2 = new ConditionStatment(Integer.toString(op2.getId()), ConditionStatment.Operator.NotEqual, "2");
-        ConditionOperator co = new ConditionOperator(ConditionOperator.Type.OR);
-        assertTrue(ce.appendElement(co, cs1));
-        ce.appendElement(co, cs2);
-        for (ConditionElement cee : ce.getConditionElements()) {
-            System.out.println("start");
-            System.out.println(cee.toString());
-            System.out.println("stop");
-        }
 
-        System.out.println(c.getGuard().getExpressionRoot().toString());
-        System.out.println(c.getGuard().getExpressionRoot().getNextElement().toString());
-        System.out.println(c.getGuard().getExpressionRoot().getNextOperator().getOperatorType().toString());
+        ce.changeExpressionRoot(left);
+        ce.appendElement(ConditionOperator.Type.OR, right);
+        //-----------------------------------------------------------------------
+        
         System.out.println(c.toString());
     }
 }
