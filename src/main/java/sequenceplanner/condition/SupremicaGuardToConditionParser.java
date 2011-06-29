3300be1d73abb2898f1bfd1a3f6442c341775a6f
patrikm <patrikm@chalmers.se>
2011-06-29 12:43:42 +0200

    Small change.

--- src/main/java/sequenceplanner/condition/SupremicaGuardToConditionParser.java
+++ src/main/java/sequenceplanner/condition/SupremicaGuardToConditionParser.java
@@ -7,7 +7,7 @@ package sequenceplanner.condition;
 public class SupremicaGuardToConditionParser extends AStringToConditionParser {
 
     private static String variable = "([^\\(\\)\\d]\\w*)"; 
-    private static String statementOperator = "([=><!][=])";
+    private static String statementOperator = "([=><!][=]|[<>])";
     private static String value = "([\\d])";
     private static String clauseOperator = "(&|\\|)";
 
