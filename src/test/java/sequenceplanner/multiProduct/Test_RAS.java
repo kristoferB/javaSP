851bf63d6d73e31e76c96e3f9ef69a5fd51c17cb
patrikm <patrikm@chalmers.se>
2011-10-31 09:11:22 +0100

    Added some extra lines for easier testing of preprocessing for RAS

--- src/test/java/sequenceplanner/multiProduct/Test_RAS.java
+++ src/test/java/sequenceplanner/multiProduct/Test_RAS.java
@@ -23,10 +23,14 @@ import static org.junit.Assert.*;
  */
 public class Test_RAS implements IAlgorithmListener {
 
-    private static final String mFilePath = "C:\\Users\\patrik\\Desktop\\";
+//    private static final String mFilePath = "C:\\Users\\patrik\\Desktop\\";
+    private static final String mFilePath = "Z:\\RAS_assembly\\Excel files for RAS models\\";
+
 //    private static final String mFileName = "FLEXAplusNoBuffers.xls";
-//    private static final String mFileName = "ProductTypesTest.xls";
-    private static final String mFileName = "Benchmark2_RAS.xls";
+//    private static final String mFileName = "FLEXAplus.xls";
+    private static final String mFileName = "ProductTypesTest.xls";
+//    private static final String mFileName = "Benchmark2_RAS.xls";
+
     private static final int mSecondsToRunSynthesisThread = 30;
 
     @BeforeClass
